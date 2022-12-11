6137a0526b8ab08739b0b6b165edf64bbef62257
boris.barkovski <boris.barkovski@gmail.com>
2022-12-12 00:47:52 +0300

    Task2

--- 1_lesson/HomeWork/1/Program.cs
+++ 1_lesson/HomeWork/1/Program.cs
@@ -1,10 +1,8 @@
 ﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
 //какое число большее, а какое меньшее.
 
-string a = Console.ReadLine();
-string b = Console.ReadLine();
-int a_1 = int.Parse(a);
-int b_1 = int.Parse(b);
+int a_1 = int.Parse(Console.ReadLine());
+int b_1 = int.Parse(Console.ReadLine());
 if (a_1 > b_1)
 {
     Console.WriteLine($"Число {a_1} больше числа {b_1}");
---
+++ 1_lesson/HomeWork/2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+int a_1 = int.Parse(Console.ReadLine());
+int a_2 = int.Parse(Console.ReadLine());
+int a_3 = int.Parse(Console.ReadLine());
+
+if (a_1 < a_2);
+{
+    a_1 = a_2;
+}
+if (a_1 < a_3);
+{
+    a_1 = a_3;
+}
+Console.WriteLine($"Число {a_1} максимальное.");
