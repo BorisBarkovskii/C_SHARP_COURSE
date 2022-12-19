3c06fb1fcef3e69acf70fb5d486df925b844f9e5
boris.barkovski <boris.barkovski@gmail.com>
2022-12-19 18:00:54 +0300

    Task 3_3

---
+++ 3_lesson/3_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая прнимает на вхд число (N) и выдает таблицу
+// квадратов чисел от 1 до N.
+
+void Chet(int N)
+{
+    int i = 0;
+    double pere;
+    while (i <= N)
+   {
+        i += 1;
+        pere = Math.Pow(i,2);
+        Console.Write($"{pere},");
+   }
+}
+Console.WriteLine("Введите число:");
+int chislo = int.Parse(Console.ReadLine());
+Chet(chislo);
