81356d09d3f084befb8ba74fd3a6a3a72fe65266
boris.barkovski <boris.barkovski@gmail.com>
2022-12-21 01:33:13 +0300

    Task 5_2

---
+++ 5_lesson/5_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли
+// заданное число в массиве.
+void Print(int[] array)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+int[] Mass(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(1, 100);
+    return array;
+}
+string Search(int[] array, int num)
+{
+    for(int i = 0; i< array.Length; i++)
+    {
+        if(num == array[i])
+            return "Да";
+    }
+    return "Нет";
+}
+int[] array_1 = Mass(int.Parse(Console.ReadLine()));
+Print(array_1);
+Console.WriteLine(Search(array_1, int.Parse(Console.ReadLine())));
