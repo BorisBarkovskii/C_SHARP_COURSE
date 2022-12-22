87ed6ada81463ea22554529fe3b839bee287f178
boris.barkovski <boris.barkovski@gmail.com>
2022-12-22 10:57:26 +0300

    Task 6_2

---
+++ 6_lesson/6_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// 2. Напишите программу, которая будет
+//    преобразовывать десятичное число в двоичное.
+
+string Bin(int num)
+{
+    string res = "";
+
+    while (num > 0)
+    {
+        res = num % 2 + res;
+        num /= 2;
+    }
+    return res;
+}
+
+Console.WriteLine(Bin(int.Parse(Console.ReadLine())));
